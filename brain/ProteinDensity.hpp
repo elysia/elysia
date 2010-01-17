5ef15bd9fa8acb62da49488a699f58d2748d62dd
hellcatv <hellcatv@e0ac87d7-b42b-0410-9034-8248a05cb448>
2010-01-17 10:40:58 +0000

    added protein density map for densities within a single neuron as well as the surrounding env
    git-svn-id: https://elysia.svn.sourceforge.net/svnroot/elysia/trunk@776 e0ac87d7-b42b-0410-9034-8248a05cb448

--- brain/ProteinDensity.hpp
+++ brain/ProteinDensity.hpp
@@ -1,1 +1,12 @@
-class ProteinDensity {    typedef std::map<Elysia::Genome::Protein::Effect,float> ProteinDensityMap;            ProteinDensityMap mIntrinsicProteins;    ProteinEnvironment *mExtrinsicProteins;public:  float getProteinDensity(const Elysia::Genome::ProteinEffect&);  std::vector<ProteinDensityMap::value_type> getCompleteProteinDensity();};
+
+class ProteinDensity {
+    typedef std::map<Elysia::Genome::Protein::Effect,float> ProteinDensityMap;
+    
+    
+    ProteinDensityMap mIntrinsicProteins;
+    ProteinEnvironment *mExtrinsicProteins;
+public:
+  float getProteinDensity(const Elysia::Genome::ProteinEffect&);
+  std::vector<ProteinDensityMap::value_type> getCompleteProteinDensity();
+};
+
