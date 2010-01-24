522f1ea6717013265b3ca0440b5bba03bced7919
hellcatv <hellcatv@e0ac87d7-b42b-0410-9034-8248a05cb448>
2010-01-24 21:28:15 +0000

    simple test
    git-svn-id: https://elysia.svn.sourceforge.net/svnroot/elysia/trunk@781 e0ac87d7-b42b-0410-9034-8248a05cb448

---
+++ brain/main.cpp
@@ -0,0 +1,9 @@
+#include "Platform.hpp"
+#include "genome.pb.h"
+int main() {
+    Elysia::Vector3f test(0,1,2);
+    std::tr1::unordered_map<Elysia::String,Elysia::Vector3f> bleh;
+    bleh["ahoy"]=test;
+    Elysia::Genome::Genome genes;
+    return 0;
+}
