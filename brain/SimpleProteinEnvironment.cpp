3e6c2ac63e3b9464eec4190773e6970d00d577ec
hellcatv <hellcatv@e0ac87d7-b42b-0410-9034-8248a05cb448>
2010-02-05 05:19:10 +0000

    changed mac linefeeds to unix
    git-svn-id: https://elysia.svn.sourceforge.net/svnroot/elysia/trunk@796 e0ac87d7-b42b-0410-9034-8248a05cb448

--- brain/SimpleProteinEnvironment.cpp
+++ brain/SimpleProteinEnvironment.cpp
@@ -1,1 +1,41 @@
-float SimpleProteinEnvironment::getProteinDensity(const Vector3 &location, const Elysia::Genome::ProteinEffect&effect) {  float density=0;  for (std::vector<ProteinDeposit>::iterator i=mProteins.begin(),ie=mProteins.end();i!=ie;++i) {    if (i->getEffect()==effect&&i->getBoundingBox().contanis(location)) {        density+=i->currentDensity;    }  }  return density;}std::vector<std::pair<Elysia::Genome::ProteinEffect, float> > SimpleProteinEnvironment::getCompleteProteinDensity(const Vector3& location){  std::vector<std::pair<Elysia::Genome::ProteinEffect, float> >proteins;  for (std::vector<ProteinDeposit>::iterator i=mProteins.begin(),ie=mProteins.end();i!=ie;++i) {    if (i->getBoundingBox().contanis(location)) {        proteins.push_back(std::pair<Elysia::Genome::ProteinEffect,float>(i->getEffect(),density);    }  }  return proteins;}//"Complicated" function to update the soup for the next time iterationvoid SimpleProteinEnvironment::ProteinZone::updateSoupToNextSoup(const float age){}//Split large zone definitions into smaller component zones for calculationsstd::vector<ProteinZone> SimpleProteinEnvironment::ZoneIntersection(const ProteinZone &zone1,                                                                     const ProteinZone &zone2){}//Zone management functions to add and remove zones from the main listvoid SimpleProteinEnvironment::addZone( std::vector<ProteinZone> mSubZoneList,                                         std::vector<ProteinZone> mMainZoneList){}void SimpleProteinEnvironment::removeZone(  std::vector<ProteinZone> mSubZoneList,                                             std::vector<ProteinZone> mMainZoneList){}//Function to find the zone that a single point reside inProteinZone &SimpleProteinEnvironment::resideInZones(const Vector3f queryPoint, std::vector<ProteinZone> mMainZoneList){}
+float SimpleProteinEnvironment::getProteinDensity(const Vector3 &location, const Elysia::Genome::ProteinEffect&effect) {
+  float density=0;
+  for (std::vector<ProteinDeposit>::iterator i=mProteins.begin(),ie=mProteins.end();i!=ie;++i) {
+    if (i->getEffect()==effect&&i->getBoundingBox().contanis(location)) {
+        density+=i->currentDensity;
+    }
+  }
+  return density;
+}
+
+std::vector<std::pair<Elysia::Genome::ProteinEffect, float> > SimpleProteinEnvironment::getCompleteProteinDensity(const Vector3& location){
+  std::vector<std::pair<Elysia::Genome::ProteinEffect, float> >proteins;
+  for (std::vector<ProteinDeposit>::iterator i=mProteins.begin(),ie=mProteins.end();i!=ie;++i) {
+    if (i->getBoundingBox().contanis(location)) {
+        proteins.push_back(std::pair<Elysia::Genome::ProteinEffect,float>(i->getEffect(),density);
+    }
+  }
+  return proteins;
+}
+
+//"Complicated" function to update the soup for the next time iteration
+void SimpleProteinEnvironment::ProteinZone::updateSoupToNextSoup(const float age){
+}
+
+//Split large zone definitions into smaller component zones for calculations
+std::vector<ProteinZone> SimpleProteinEnvironment::ZoneIntersection(const ProteinZone &zone1, 
+                                                                    const ProteinZone &zone2){
+}
+
+//Zone management functions to add and remove zones from the main list
+void SimpleProteinEnvironment::addZone( std::vector<ProteinZone> mSubZoneList, 
+                                        std::vector<ProteinZone> mMainZoneList){
+}
+
+void SimpleProteinEnvironment::removeZone(  std::vector<ProteinZone> mSubZoneList, 
+                                            std::vector<ProteinZone> mMainZoneList){
+}
+
+//Function to find the zone that a single point reside in
+ProteinZone &SimpleProteinEnvironment::resideInZones(const Vector3f queryPoint, std::vector<ProteinZone> mMainZoneList){
+}
