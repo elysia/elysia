9aefe3a702a5ce3aacd260603e16d7a222c3bf22
hellcatv <hellcatv@e0ac87d7-b42b-0410-9034-8248a05cb448>
2010-06-11 05:27:38 +0000

    added a test for SimpleProteinEnvironment
    git-svn-id: https://elysia.svn.sourceforge.net/svnroot/elysia/trunk@867 e0ac87d7-b42b-0410-9034-8248a05cb448

--- brain/plugins/vis/vis_plugin.cpp
+++ brain/plugins/vis/vis_plugin.cpp
@@ -1,9 +1,9 @@
 #include "../../Platform.hpp"
-
 static int core_plugin_refcount = 0;
 
 SIRIKATA_PLUGIN_EXPORT_C void init() {
     core_plugin_refcount++;
+    
 }
 
 SIRIKATA_PLUGIN_EXPORT_C void destroy() {
--- brain/test.cpp
+++ brain/test.cpp
@@ -59,11 +59,70 @@ void testTwoConnectedNeurons() {
 
 }
 
+void testProteinEnvironment() {
+   using namespace Elysia::Genome;
+   Elysia::Genome::Genome twoOverlappingGenes;
+   Elysia::Genome::Chromosome *father=twoOverlappingGenes.mutable_fathers();
+   Elysia::Genome::Gene firstGene;
+   Elysia::Genome::Protein firstProtein;
+   firstProtein.set_protein_code(GROW_NEURON);//so we can easily identify where
+   firstProtein.set_density(0.125);   
+   Elysia::Genome::Protein firstAuxProtein;
+   firstProtein.set_protein_code(GROW_LEAF);//so we see that they are additive
+   firstProtein.set_density(0.25);
+   *firstGene.add_external_proteins()=firstProtein;
+   *firstGene.add_external_proteins()=firstAuxProtein;
+   Elysia::Genome::TemporalBoundingBox firstRegion;
+   firstRegion.set_minx(0);
+   firstRegion.set_miny(0);
+   firstRegion.set_minz(0);
+   firstRegion.set_maxx(2);
+   firstRegion.set_maxy(2);
+   firstRegion.set_maxz(2);
 
+   *firstGene.add_bounds()=firstRegion;
+   Elysia::Genome::Gene secondGene;
+   Elysia::Genome::Protein secondProtein;
+   secondProtein.set_protein_code(GROW_LEAF);
+   secondProtein.set_density(0.5);
+   *secondGene.add_external_proteins()=secondProtein;
+   Elysia::Genome::TemporalBoundingBox secondRegion;
+   secondRegion.set_minx(-1);
+   secondRegion.set_miny(-1);
+   secondRegion.set_minz(-1);
+   secondRegion.set_maxx(1);
+   secondRegion.set_maxy(1);
+   secondRegion.set_maxz(1);
+   *secondGene.add_bounds()=secondRegion;
+   *father->add_genes()=firstGene;
+   *father->add_genes()=secondGene;
+   
+   ProteinEnvironment * pe=new SimpleProteinEnvironment;
+   pe->initialize(twoOverlappingGenes);
+   std::vector<std::pair<Elysia::Genome::Effect, float> > combinedResult=pe->getCompleteProteinDensity(Vector3f(.5,.5,.5));
+   //check that firstResult matches expectations
+   std::vector<std::pair<Elysia::Genome::Effect, float> > firstResult=pe->getCompleteProteinDensity(Vector3f(1.5,1.5,1.5));
+   //check that secondResult matches expectations
+   std::vector<std::pair<Elysia::Genome::Effect, float> > secondResult=pe->getCompleteProteinDensity(Vector3f(-.5,-.5,-.5));
+   assert(combinedResult.size()==2);
+   assert(firstResult.size()==2);
+   assert(secondResult.size()==1);
+   assert((combinedResult[0].first==GROW_LEAF&&combinedResult[0].second==.75)||
+          (combinedResult[1].first==GROW_LEAF&&combinedResult[1].second==.75));
+   assert((combinedResult[0].first==GROW_NEURON&&combinedResult[0].second==.125)||
+          (combinedResult[1].first==GROW_NEURON&&combinedResult[1].second==.125));
+   assert((firstResult[0].first==GROW_LEAF&&firstResult[0].second==.25)||
+          (firstResult[1].first==GROW_LEAF&&firstResult[1].second==.25));
+   assert((firstResult[0].first==GROW_NEURON&&firstResult[0].second==.125)||
+          (firstResult[1].first==GROW_NEURON&&firstResult[1].second==.125));
+   assert(secondResult[0].first==GROW_LEAF&&secondResult[0].second==.5);
+   delete pe;
+}
 }
 
 int runtest(){
     Elysia::testTwoConnectedNeurons();
+    Elysia::testProteinEnvironment();
 	getchar();
 	return 1;
 	
