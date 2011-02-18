5e780ee437be2650b50b436a9d153282b4bfa2fe
Daniel Reiter Horn <danielrh@users.sourceforge.net>
2011-02-17 21:53:34 -0800

    fixed compile error: C style arrays do not have copy constructors so you have to use the really verbose methods to copy them around. Better to use a bounding box class

--- brain/AnnSpace.cpp
+++ brain/AnnSpace.cpp
@@ -9,7 +9,9 @@ namespace Elysia{
 		return this;
 	}
 	void AnnSpace::setBoundingBox(float box[2][2]){
-		mBoundingBox = newBox;
+        for (int i=0;i<2;++i)
+            for (int j=0;j<2;++j)
+                mBoundingBox[i][j]=box[i][j];
 	}
 	
 	//}
